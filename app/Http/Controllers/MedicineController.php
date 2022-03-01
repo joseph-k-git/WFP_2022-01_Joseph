af8b68b22b20dfc91ec91a3c4b608c7af75088d8
Joseph Kristiano <o.joseph.k@gmail.com>
2022-03-01 12:00:33 +0700

    W04 - Query & Controller

---
+++ app/Http/Controllers/MedicineController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Medicine;
+use Illuminate\Http\Request;
+use DB;
+
+class MedicineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Raw Query
+        //$result = DB::select(DB::raw("SELECT * FROM medicines"));
+
+        // Fluent Query Builder
+        //$result = DB::table("medicines")->get();
+
+        // Eloquent ORM
+        $result = Medicine::all();
+
+        //dd($result);
+
+        // Compact. Nama harus sama.
+        //return view('medicine.index', compact('result'));
+
+        // Array. Bisa berbeda nama untuk diterima di view.
+        return view('medicine.index', ['result' => $result]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Medicine $medicine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Medicine  $medicine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Medicine $medicine)
+    {
+        //
+    }
+}
