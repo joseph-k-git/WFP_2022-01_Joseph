e428dd09901631573342774f731c8beb4b23aaa1
Joseph Kristiano <o.joseph.k@gmail.com>
2022-04-01 04:08:20 +0700

    W06 - Templating - Extends Layout

--- resources/views/layout/conquer.blade.php
+++ resources/views/layout/conquer.blade.php
@@ -26,20 +26,20 @@ License: You must have a valid license purchased only from themeforest(the above
 <meta name="MobileOptimized" content="320">
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
 <!-- END GLOBAL MANDATORY STYLES -->
 <!-- BEGIN THEME STYLES -->
-<link href="assets/css/style-conquer.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style-responsive.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/plugins.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
-<link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/style-conquer.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/style.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/style-responsive.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/themes/default.css" rel="stylesheet" type="text/css" id="style_color"/>
+<link href="/assets/css/custom.css" rel="stylesheet" type="text/css"/>
 <!-- END THEME STYLES -->
-<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="shortcut icon" href="/favicon.ico"/>
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
@@ -51,7 +51,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <!-- BEGIN LOGO -->
 	<div class="page-logo">
             <a href="index.html">
-                <img src="assets/img/logo.png" alt="logo"/>
+                <img src="/assets/img/logo.png" alt="logo"/>
             </a>
         </div>
         <form class="search-form search-form-header" role="form" action="index.html">
@@ -63,7 +63,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!-- END LOGO -->
         <!-- BEGIN RESPONSIVE MENU TOGGLER -->
         <a href="javascript:;" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <img src="assets/img/menu-toggler.png" alt=""/>
+        <img src="/assets/img/menu-toggler.png" alt=""/>
         </a>
         <!-- END RESPONSIVE MENU TOGGLER -->
         <!-- BEGIN TOP NAVIGATION MENU -->
@@ -190,7 +190,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li>
                                 <a href="inbox.html?a=view">
                                 <span class="photo">
-                                <img src="./assets/img/avatar2.jpg" alt=""/>
+                                <img src="/assets/img/avatar2.jpg" alt=""/>
                                 </span>
                                 <span class="subject">
                                 <span class="from">
@@ -205,7 +205,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li>
                                 <a href="inbox.html?a=view">
                                 <span class="photo">
-                                <img src="./assets/img/avatar3.jpg" alt=""/>
+                                <img src="/assets/img/avatar3.jpg" alt=""/>
                                 </span>
                                 <span class="subject">
                                 <span class="from">
@@ -220,7 +220,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li>
                                 <a href="inbox.html?a=view">
                                 <span class="photo">
-                                <img src="./assets/img/avatar1.jpg" alt=""/>
+                                <img src="/assets/img/avatar1.jpg" alt=""/>
                                 </span>
                                 <span class="subject">
                                 <span class="from">
@@ -235,7 +235,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li>
                                 <a href="inbox.html?a=view">
                                 <span class="photo">
-                                <img src="./assets/img/avatar2.jpg" alt=""/>
+                                <img src="/assets/img/avatar2.jpg" alt=""/>
                                 </span>
                                 <span class="subject">
                                 <span class="from">
@@ -250,7 +250,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             <li>
                                 <a href="inbox.html?a=view">
                                 <span class="photo">
-                                <img src="./assets/img/avatar3.jpg" alt=""/>
+                                <img src="/assets/img/avatar3.jpg" alt=""/>
                                 </span>
                                 <span class="subject">
                                 <span class="from">
@@ -411,7 +411,7 @@ License: You must have a valid license purchased only from themeforest(the above
             <!-- BEGIN USER LOGIN DROPDOWN -->
             <li class="dropdown user">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                <img alt="" src="assets/img/avatar3_small.jpg"/>
+                <img alt="" src="/assets/img/avatar3_small.jpg"/>
                 <span class="username">
                 Nick </span>
                 <i class="fa fa-angle-down"></i>
@@ -892,18 +892,18 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- END FOOTER -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
 <!-- BEGIN CORE PLUGINS -->
-<script src="assets/plugins/jquery-1.11.0.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/jquery-1.11.0.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
 <!-- IMPORTANT! Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-<script src="assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="assets/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="/assets/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="assets/scripts/app.js"></script>
+<script src="/assets/scripts/app.js"></script>
 <!-- END PAGE LEVEL SCRIPTS -->
 <script>
 jQuery(document).ready(function() {       
--- resources/views/medicine/show.blade.php
+++ resources/views/medicine/show.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Medicines</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+@extends('layout.conquer')
 
+@section('content')
 <div class="container">
   <h2>Daftar Obat</h2>
   <table class="table">
@@ -47,6 +38,4 @@
     </tbody>
   </table>
 </div>
-
-</body>
-</html>
+@endsection
